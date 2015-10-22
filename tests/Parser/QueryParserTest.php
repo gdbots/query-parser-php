6f938d8bc22394e46fbd2a6e4315bce2468f5864
Michael H. Arieli <michaelharieli@gmail.com>
2015-10-22 05:44:11 -0700

    RE #1: Added unit tests

--- tests/Parser/QueryParserTest.php
+++ tests/Parser/QueryParserTest.php
@@ -56,6 +56,57 @@ class QueryParserTest extends \PHPUnit_Framework_TestCase
         ];
     }
 
+    /**
+     * @dataProvider getTestParseWithPrintoutDataprovider
+     */
+    public function testParseQuery($query, $expected)
+    {
+        $this->parser->readString($query, true);
+        $query = $this->parser->parse();
+
+        $output =  $this->getPrintContent($query);
+        $output = preg_replace("/[\r\n]+/", '', $output);
+        $output = preg_replace('/\s+/', '', $output);
+
+        $this->assertEquals($expected, $output);
+    }
+
+    public function getTestParseWithPrintoutDataprovider()
+    {
+        return [
+            ['##one', 'Hashtag>Word:one'],
+            ['#one #two #three', 'Or>Hashtag>>Word:one>Hashtag>>Word:two>Hashtag>>Word:three'],
+            ['#one#two##three', 'Or>Hashtag>>Word:one>Hashtag>>Word:two>Hashtag>>Word:three'],
+            ['#one!', 'Hashtag>Word:one'],
+            ['"#one^7 #two#three ##four!"', 'Phrase:#one^7#two#three##four!'],
+            ['a^b', 'Or>Word:a>Word:b'],
+            ['a^^2', 'Term:a^2'],
+            ['"abc"^def', 'Or>Phrase:abc>Word:def'],
+            ['"abc"^2"def ^ghi"jkl^mno^8', 'Or>Term:abc^2>Phrase:def^ghi>Word:jkl>Term:mno^8'],
+            ['abc^2def', 'Or>Term:abc^2>Word:def'],
+            ['#a^2', 'Term:^2>Hashtag>>Word:a'],
+            ['a#b', 'Or>Word:a>Hashtag>>Word:b'],
+            ['+a-b-c -d-e+f', 'Or>IncludeTerm>>Word:a-b-c>ExcludeTerm>>Word:d>Word:e>Word:f'],
+            ['+a-b - c -d - e+f', 'Or>IncludeTerm>>Word:a-b>Phrase:->Word:c>ExcludeTerm>>Word:d>Phrase:->Word:e>Word:f'],
+            ['"abc""def""ghi', 'Or>Phrase:abc>Phrase:def>Word:ghi'],
+            ['"abc"def', 'Or>Phrase:abc>Word:def'],
+            ['"abc"def"', 'Or>Phrase:abc>Word:def'],
+            ['abc"def', 'Or>Word:abc>Word:def'],
+            ['abc"def ghi"@j"@k l', 'Or>Word:abc>Phrase:defghi>Mention>>Word:j>Mention>>Word:k>Word:l'],
+            ['#a#b@c @d#e', 'Or>Hashtag>>Word:a>Hashtag>>Word:b>Mention>>Word:c>Mention>>Word:d>Hashtag>>Word:e'],
+            ['(a b)^2', 'Or>Word:a>Term:b^2'],
+            ['+(a b c)-(d e f)^2', 'Or>IncludeTerm>>Word:a>Word:b>Word:c>Word:d>Word:e>Term:f^2'],
+            ['a b:', 'Or>Word:a>Word:b'],
+            ['http://a.com a:>500', 'Or>Word:http://a.com>Term:a:>500'],
+            ['a (b/c d)^2 Father and Daughter', 'Or>Word:a>Word:b>Word:c>Term:d^2>Word:Father>Word:and>Word:Daughter'],
+            ['a:>b^2abc', 'Or>Term:^2>>Term:a:>b>Word:abc'],
+            ['a + b', 'Or>Word:a>Phrase:+>Word:b',],
+            ['+(a:>b)-c:>d -e:<f', 'Or>IncludeTerm>>Term:a:>b>Term:c:>d>ExcludeTerm>>Term:e:<f'],
+            ['a:(a b)^2', 'Or>Term:a:a>Term:b^2'],
+            [' #+a #-b', 'Or>Word:a>Word:b']
+        ];
+    }
+
     public function testParseTextWithUnclosedQuotes()
     {
         $this->parser->readString('"phrase');
