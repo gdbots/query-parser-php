9db2ec091ede88a8937f974a5da4b8cfaf0bb30a
Michael H. Arieli <michaelharieli@gmail.com>
2015-11-03 13:45:48 -0800

    Added hack to solve last element type

--- src/QueryLexer.php
+++ src/QueryLexer.php
@@ -458,7 +458,7 @@ class QueryLexer
         $input = preg_replace('/(\()/', '$1 ', $input);
         $input = preg_replace('/(\))/', ' $1', $input);
 
-        $this->input = $input;
+        $this->input = $input.' '; // hack: to solve last element type
         $this->processed = '';
         $this->position = 0;
     }
